51f5f85cdf2654facf5ed0a632d9620db229b714
waffiqaziz <waffiq.a2002@gmail.com>
2021-04-06 16:07:57 +0700

    create project
    checking project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # prak_pbo_tugas1
+ @author Waffiq Aziz / 123190070
  hitung (luas,keliling,volume) bangun datar maupun bangun ruang
---
+++ menghitung/src/com/menghitung/Main.java
@@ -0,0 +1,7 @@
+package com.menghitung;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("HAI");
+  }
+}
